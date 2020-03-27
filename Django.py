124de2227ebfc9fc83726c22bde02d43f6af1e14
Jayesh321 <52677251+Jayesh321@users.noreply.github.com>
2020-03-27 16:57:19 +0530

    Add files via upload
    first commit

---
+++ Django.py
@@ -0,0 +1,26 @@
+# How to start the server:
+# Python manage.py runserver
+
+# Then we can view the  page on - http://127.0.0.1:8000/
+
+# How to start project in django
+# django-admin.py startproject projectname
+
+# To create an app, type this in the site directory:
+# python manage.py startapp appname
+
+#How to start the interpreter with django template settings
+#python manage.py shell
+
+# How to find out the django version and find out if django is running on your server
+# Import django
+# django.VERSION
+
+# DATA BASE:
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}'''
